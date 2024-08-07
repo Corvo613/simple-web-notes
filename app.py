1afe6503ada2d84d2385c03edf55f4b1381d3b80
Corvo613 <egor.kreslin@gmail.com>
2024-08-08 00:53:16 +0300

    Edited some titles

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  return 'Hello, World!'
+
+
+@app.route('/hello/<name>')
+def hello(name):
+  return render_template('hello.html', name=name)
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
