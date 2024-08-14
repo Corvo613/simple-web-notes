4d4371e2b7e42718ad23c10f60b8497b28a79136
Corvo613 <egor.kreslin@gmail.com>
2024-08-15 00:23:50 +0300

    Added code for SQLite connect

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///simple-web-notes.db'
+db = SQLAlchemy(app)
+
+class Note(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100))
+  content = db.Column(db.Text)
+
+  def __repr__(self):
+    return '<Note %r>' % self.id
 
 
 @app.route('/')
